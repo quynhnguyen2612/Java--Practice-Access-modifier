2e98d82a552351ce2e345ab7b5e75585c7de645a
quynhnguyen2612 <vanquynh336@gmail.com>
2023-12-10 17:57:44 +0700

    Java--Practice-Access-modifier

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+       public static void main(String[] args){
+              Test student = new Test();
+              student.setName("quynh");
+              student.setClasses("C09");
+       }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    private String name;
+    private String classes;
+
+    public Test() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
